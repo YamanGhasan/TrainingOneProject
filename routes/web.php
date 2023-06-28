1a48c9dde3084824fbf64c8662ee9cab784549f4
YamanGhasan <116804915+YamanGhasan@users.noreply.github.com>
2023-06-28 13:43:39 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ Route::get('/', function () {
     return "Welcome here is Product list <a href='" . route('products.index') . "'>Go to Products</a>";
 });
 
-
-Route::get('/products', [ProductController::class, 'show'])->name('products.index');
+ 
+Route::get('/products', [ProductController::class, 'show'])->name('products.index'); 
 Route::get('/products/add', [ProductController::class, 'add'])->name('products.add');
 Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
 Route::delete('/products/{id}', [ProductController::class, 'delete'])->name('products.delete');
