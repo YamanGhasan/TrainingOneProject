a378856b38f6256c6473a05e42e98d43088bdeaf
YamanGhasan <116804915+YamanGhasan@users.noreply.github.com>
2023-07-01 21:18:44 +0300

    Update addProduct.blade.php

--- resources/views/addProduct.blade.php
+++ resources/views/addProduct.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Product</title>
-    <style>
+    <style> 
         body {
             font-family: Arial, sans-serif;
             padding: 20px;
