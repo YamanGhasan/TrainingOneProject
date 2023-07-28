a58fd35e94048236f4c64808144979018d461b96
YamanGhasan <116804915+YamanGhasan@users.noreply.github.com>
2023-07-28 21:32:13 +0300

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,7 +10,7 @@ class Product extends Model
     use HasFactory;    
  
     protected $table = 'products';  
-    protected $fillable = ['name', 'price'];
+    protected $fillable = ['name', 'price']; 
       
     public $timestamps = true;
 }
