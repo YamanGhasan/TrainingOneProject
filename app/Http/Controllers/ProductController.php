a2ea105821b321f4f2cb3b320964b0167dd1c4b0
YamanGhasan <116804915+YamanGhasan@users.noreply.github.com>
2023-08-01 13:12:13 +0300

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     public function show()
-    {
+    { 
         $products = Product::all();
 
         return view('productList')->with('products', $products);
